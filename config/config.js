193cc6ab8667dba1f1a527e958b42bc6e40af9cf
Nikola Simovic <simovic.nikola90@gmail.com>
2017-03-16 15:10:54 +0100

    Small change to the config file

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // "dbname": "mongodb://nikola1970:grobari@ds129050.mlab.com:29050/bookmarks-db",
     "dbname": "mongodb://localhost:27017/bookmarks",
     "secret": "Grobarijug@1970"
 };
