218081b50f6b09fcdefddede1e68b16c2fdecb5f
Nikola Simovic <simovic.nikola90@gmail.com>
2017-03-27 09:42:23 +0200

    Initial

--- public/app/app.js
+++ public/app/app.js
@@ -1,37 +1,59 @@
 (function(){
 
     config.$inject = ["$routeProvider", "$httpProvider", "$locationProvider"];
+    run.$inject = ["$location", "UsersFactory", "$rootScope"];
 
-    angular.module("bookmarkApp", ["ngRoute", "ngAnimate", "toastr"]).config(config);
+    angular.module("bookmarkApp", ["ngRoute", "ngAnimate", "toastr"]).config(config).run(run);
+
+    function run($location, UsersFactory, $rootScope) {
+        $rootScope.$on("$routeChangeStart", function(event, next, current){
+            if (next.$$route.authenticated) {
+               if (!UsersFactory.isLoggedIn()) {
+                   event.preventDefault();
+                   $location.path("/login");
+               }
+            } else if (!next.$$route.authenticated) {
+                if (UsersFactory.isLoggedIn()) {
+                    event.preventDefault();
+                    $location.path("/");
+                }
+            }
+        });
+    }
 
     function config($routeProvider, $httpProvider, $locationProvider) {
-
-
 
         $httpProvider.interceptors.push("Interceptor");
 
         $routeProvider
             .when("/", {
-                templateUrl: "./app/templates/home.html"
+                templateUrl: "./app/templates/home.html",
+                authenticated: true
             })
             .when("/login", {
                 templateUrl: "./app/templates/login.html",
                 controller: "LoginController",
-                controllerAs: "login"
+                controllerAs: "login",
+                authenticated: false
             })
             .when("/signup", {
                 templateUrl: "./app/templates/signup.html",
                 controller: "SignupController",
-                controllerAs: "signup"
+                controllerAs: "signup",
+                authenticated: false
             })
             .when("/logout", {
-                templateUrl: "./app/templates/logout.html"
+                templateUrl: "./app/templates/logout.html",
+                authenticated: false
             })
             .otherwise({
-                redirecTo: "/"
+                redirectTo: "/"
             });
 
-            $locationProvider.html5Mode(true);
+            // $locationProvider.html5Mode({
+            //     enabled: true,
+            //     requireBase: false
+            // });
         
     }
 
--- public/app/controllers/main.controller.js
+++ public/app/controllers/main.controller.js
@@ -8,19 +8,22 @@
             if (UsersFactory.isLoggedIn()) {
                 console.log("Success: User is logged in!");
                 UsersFactory.getUser().then(function(data) {
-                    self.user = data.data.username;
+                    if (data.success === false) {
+                        UsersFactory.logout();
+                    } else {
+                        self.user = data.data.username;
+                    }
                 });
             } else {
                 console.log("Failure: User is not logged in!");
                 self.user = "";
-                $location.path("/login");
             }
         });
 
         this.logout = function(){
             UsersFactory.logout();
             $timeout(function(){
-                $location.path("/");
+                $location.path("/login");
             }, 1500);
         };
 
